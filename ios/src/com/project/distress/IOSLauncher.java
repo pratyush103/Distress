f0f5e4f8907b3c7076890094f81bfa7266f1e3f1
Pratyush Landekar <pratyushlandekar103@gmail.com>
2024-01-28 15:24:47 +0530

    Initial commit

---
+++ ios/src/com/project/distress/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.project.distress;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.project.distress.DistressGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new DistressGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
